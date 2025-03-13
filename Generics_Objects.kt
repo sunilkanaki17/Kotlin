f83301bd95962b8f01c1371749a39d94bb4c1d94
Sunil Kanaki <sunilkanaki17@gmail.com>
2025-03-13 10:25:28 +0530

    Generic_objects_enum_dataclass
    Generics allow data types to be passed as parameters to classes, enum classes define a limited set of possible values, and data classes help automatically generate some useful methods for classes.

---
+++ Generics_Objects.kt
@@ -0,0 +1,40 @@
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+    
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+    
+    override fun printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(progressText)
+}
+    
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+data class Question<T> (
+    val questionText : String,
+    val answer : T,
+    val difficulty : Difficulty
+)
+
+enum class Difficulty {
+	EASY, HARD, MEDIUM
+}
+
+fun main() {
+    Quiz().printProgressBar()
+}
